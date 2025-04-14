889c7ef9bfe0b8049aa6ffe69d4a0910e27d7168
Polina <polina.panfilowa15@mail.ru>
2025-04-14 08:28:09 +0300

    First commit

--- HW.py
+++ HW.py
@@ -1,14 +1,14 @@
-# #1
-# def task_1(a: int, b: float, c: str, d: list, e: bool):
-#     return(type(a), type(b), type(c), type(d), type(e))
-#
-# print(task_1(3, 9.2, "hello", [5, "May", True], False))
-#
-# #2
-# def task_2(a=[1, 2, 3, 5, 8, 13, 21]) -> list:
-#     print(a[0:2])
-#
-# task_2()
+#1
+def task_1(a: int, b: float, c: str, d: list, e: bool):
+    return(type(a), type(b), type(c), type(d), type(e))
+
+print(task_1(3, 9.2, "hello", [5, "May", True], False))
+
+#2
+def task_2(a=[1, 2, 3, 5, 8, 13, 21]) -> list:
+    print(a[0:2])
+
+task_2()
 
 #3
 def task_3(x: int) ->int:
